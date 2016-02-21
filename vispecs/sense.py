b9d389d4723ee062bcfb1b9afc1ef53b12f0ecf4
Alisdair Robertson <alisdair@alisdairrobertson.com>
2016-02-21 15:59:39 +1100

    Added spectrum reading to sense.py

--- vispecs/sense.py
+++ vispecs/sense.py
@@ -9,16 +9,20 @@ import picamera
 import datetime
 import socket
 
+
+IMAGE_FORMAT = '.jpeg'
+SPECTRUM_FORMAT = '.hdf5'
+DATETIME = datetime.datetime.utcnow().strftime("UTC_%d-%m-%y_%H.%M.%S")         # Current datetime as UTC
+PI_NAME = socket.gethostname()
+
 # Define the time and date as name of the file
 # The format is [pi name]__[day]-[month]-[year]_[hour].[min].[sec].jpeg
 def getFileName():
-    pi_name = socket.gethostname()                                              # Read name of this pi
-    pi_datetime = datetime.datetime.now().strftime("%d-%m-%y_%H.%M.%S")         # Get datetime
-    return pi_name + "__" + pi_datetime + ".jpeg"                               # Concat the two and return them
+    return PI_NAME + "_" + DATETIME                                             # Concat the two and return them
 
 # Take a photo with the PiCamera
 def takePhoto(imagePath):
-    print "taking a photo"
+    print "[  VISPECS  ] Taking a photo"
     try:
         fileName = getFileName()                                                # Generate a file name for the image
 
@@ -28,10 +32,10 @@ def takePhoto(imagePath):
         camera.hflip = True                                                     # Flip camera so image is not a mirror image
         camera.start_preview()                                                  # Begin a preview so we can see what's being taken
         time.sleep(3)                                                           # Let the camera warm up
-        camera.capture(imagePath + fileName)                                   # Take photo from camera and store in specified folder
+        camera.capture(imagePath + fileName + IMAGE_FORMAT)                     # Take photo from camera and store in specified folder
         camera.stop_preview()                                                   # TODO: Remove the preview in deployed sensors??
 
-        print "[  VISPECS  ] sense.py image file: " + imagePath + fileName
+        print "[  VISPECS  ] sense.py image file: " + imagePath + fileName + IMAGE_FORMAT
 
     except Exception as e:
         # TODO: If we have an exception for whatever reason, log the exception and transmit it to the server for us to find and deal with
@@ -39,5 +43,31 @@ def takePhoto(imagePath):
         print e
 
 def readSpectrum(spectrumPath):
-    # TODO: NOT YET IMPLEMENTED!!
-    print "[  VISPECS TODO ] - readSpectrum() not implemented"
+
+    import h5py                                                                 # Library for storing data in a h5 or hdf5 file
+    import numpy
+    import seabreeze.spectrometers as sb                                        # For reading spectrum data
+    fileName = getFileName()
+    try:
+        # Reading the Spectrometer
+        devices = sb.list_devices()                                                 # Get attached spectrometers
+        spec = sb.Spectrometer(devices[0])                                          # Get the STS
+        print "[  VISPECS  ] Spectrometer:" + spec.model
+        wavelengths = spec.wavelengths()
+        intensities = spec.intensities()                                            # Get arrays of the wavelengths measured and the intensities
+
+        # Storing in hdf5
+        spectFile = h5py.File(spectrumPath + fileName + SPECTRUM_FORMAT, "w")       # Get the file to write to
+        spectGrp = spectFile.create_group("grp_spectrum")                               # Create a group called 'spectrum'
+
+        spectGrp.attrs.__setitem__('attr_system', PI_NAME)                               # Store the pi/system name as an attribute of this group
+        spectGrp.attrs.__setitem__('attr_datetime', DATETIME)                            # Store the utc datetime as an attribute of this group
+
+        wavelengthsDset = spectGrp.create_dataset("dataset_wavelengths", data=wavelengths)  # Dataset for storing the wavelengths
+        intensitiesDset = spectGrp.create_dataset("dataset_intensities", data=intensities)  # Dataset for storing the intensities
+
+        spectFile.close()                                                           # Close the spectrum file
+
+    except Exception as e:
+        print "[  VISPECS  ] Exception when reading spectrum!"
+        print e
