2197664a7c908eaa9a5b72bcc826305909b02bbb
alisdairrobertson <alisdair.w.robertson@gmail.com>
2016-05-26 11:31:51 +1000

    Simplified __init__.py
    + Vispecs now uses less folders
    + code follows python guidelines

--- vispecs/__init__.py
+++ vispecs/__init__.py
@@ -1,19 +1,25 @@
 # __init__.py
-# author: alisdairrobertson | alisdairrobertson.com
-# ======
-# This is the managing script for the vispecs sensor
-# It calls the various classes to complete the sensor operations
-# It also manages the reading of the configuration file so that
-# we don't need to re-pars the file for each class
+
+""" author: alisdairrobertson | alisdairrobertson.com
+
+This is the managing script for the vispecs sensor
+It calls the various classes to complete the sensor operations
+It also manages the reading of the configuration file so that
+we don't need to re-pars the file for each class
+
+https://github.com/alisdairrobertson/rpi-vispecs
+
+"""
 
 # Imports
-import time, shutil
+import time
+import shutil
 import ConfigParser
-import subprocess
 import os
 import logging
 import socket
-from subprocess import Popen, PIPE
+from subprocess import PIPE
+from subprocess import Popen
 
 # This package Imports
 import sense
@@ -23,34 +29,38 @@ import transfer
 STORAGE = 'storage'
 FTP = 'ftp'
 
-def go():
+def vispecs_go():
+    """ Run all of the Vispecs Components"""
 
     #Initialise Logging
-    logFileName = socket.gethostname();
-    logging.basicConfig(filename= str(logFileName + ".log"),level=logging.DEBUG)
+    log_file_name = socket.gethostname()
+    logging.basicConfig(filename=str(log_file_name + ".log"),
+                        level=logging.DEBUG)
     # Log the date/time of the log
-    logging.info("+++++++ NEW WAKEUP: " + sense.getFileName())
+    logging.info("+++++++ NEW WAKEUP: " + sense.get_file_name())
 
     # Parse the configuration file
     config = ConfigParser.SafeConfigParser()
     config.read(os.path.expanduser('~') + "/vispecs.cfg")
 
     # Get info about storing files
-    imageExt = config.get(STORAGE, 'image-external')
-    spectrumExt = config.get(STORAGE, 'spectrum-external')
-    imageLocal = config.get(STORAGE, 'image-local')
-    spectrumLocal = config.get(STORAGE, 'spectrum-local')
-    sentSuffix = config.get(STORAGE, 'sent-dir')
-    extStorage = config.get(STORAGE, 'external')
-     # Flag whether to move sent files to external Storage
-    moveToExt = False
+    local_dir = os.path.expanduser('~') + '/vispecs_data/'
+    local_drop = local_dir + 'captured/'
+    sent_dir = local_dir + config.get(STORAGE, 'sent-dir')
+    ext_storage = config.get(STORAGE, 'external')
+    # Flag whether external is mounted
+    ext_mounted = False
 
     # Try to mount the USB flashdrive, writeable by any user
-    os.system("sudo mount /dev/sda1 " + extStorage + " -o umask=000")
+    os.system("sudo mount /dev/sda1 " + ext_storage + " -o umask=000")
 
      # If ethernet is connected don't run the scripts
-    (network_state, stderr) = Popen(["cat","/sys/class/net/eth0/carrier"], stdout=PIPE).communicate()
+    (network_state, stderr) = Popen(["cat", "/sys/class/net/eth0/carrier"],
+                                    stdout=PIPE).communicate()
+
     print "[  VISPECS  ] eth0 Network State: " + network_state
+    logging.info("eth0 Network State: " + network_state + " Stderr: " + stderr)
+
     if int(network_state) == 1:
         print "[  VISPECS  ] Execution cancelled, Ethernet connection detected\n"
         exit(1)
@@ -71,51 +81,43 @@ def go():
     print "[  VISPECS  ] Running scripts\n"
 
     # Make sure we use the USB if it is mounted
-    if (os.path.ismount(extStorage)):
+    if os.path.ismount(ext_storage):
         logging.info("USB Was mounted")
-        imagePath = imageExt
-        spectrumPath = spectrumExt
-        moveToExt = True
-
-        # If USB is mounted, make sure directories exist
-        if not os.path.exists(imageExt + sentSuffix):
-            os.makedirs(imageExt + sentSuffix)
-        if not os.path.exists(spectrumExt + sentSuffix):
-            os.makedirs(spectrumExt + sentSuffix)
-        logging.info("created USB dirs are: "+ imagePath + spectrumPath)
+        ext_mounted = True
 
     # Otherwise use internal storage locations
     else:
         logging.info("No USB, Falling back to internal storage")
-        imagePath = imageLocal
-        spectrumPath = spectrumLocal
 
     # Make sure that local directories exist
-    if not os.path.exists(imageLocal + sentSuffix):
-        os.makedirs(imageLocal + sentSuffix)
-    if not os.path.exists(spectrumLocal + sentSuffix):
-        os.makedirs(spectrumLocal + sentSuffix)
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
+    if not os.path.exists(local_drop):
+        os.makedirs(local_drop)
+    if not os.path.exists(local_dir + sent_dir):
+        os.makedirs(local_dir + sent_dir)
 
     # Take photograph & reading
-    sense.takePhoto(imagePath, logging)
-    sense.readSpectrum(spectrumPath, logging)
+    sense.take_photo(local_drop, logging)
+    sense.sample_spectrum(local_drop, logging)
 
     # Get the FTP info
-    ftpServer = config.get(FTP, 'ftp-server')
-    ftpLogServer = config.get(FTP, 'ftp-log-server')
-    ftpUser = config.get(FTP, 'ftp-username')
-    ftpPass = config.get(FTP, 'ftp-password')
-    ftpDirectory = config.get(FTP, 'ftp-directory')
+    ftp_server = config.get(FTP, 'ftp-server')
+    # Unused ftp user and pass
+    # ftp_user = config.get(FTP, 'ftp-username')
+    # ftp_pass = config.get(FTP, 'ftp-password')
+    ftp_directory = config.get(FTP, 'ftp-directory')
 
     # Make the FTP transfer
-    transfer.make(ftpServer, ftpUser, ftpPass, ftpDirectory,
-        imageLocal, imageExt, spectrumLocal, spectrumExt,
-        sentSuffix, moveToExt, logging, ftpLogServer)
+    transfer.make(ftp_server, ftp_directory,
+                  (local_dir, local_drop, sent_dir),
+                  (ext_storage, ext_mounted), logging)
 
     print "[  VISPECS  ] Scripts complete, shutting down."
     logging.info("Issuing shutdown command")
     shutdown_pi()
 
 def shutdown_pi():
+    """ Completely shutdown the pi, which means the sleepypi will kill power """
     print "shutdown command reached"
     os.system("sudo shutdown now")
